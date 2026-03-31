5b8955026d27c35325000d19e73570af481bade8
markmazzone-propriOS <mark.mazzone@proprieta.co>
2026-03-30 20:49:36 -0400

    Fix AgentTutorial skip/close buttons

--- src/components/Agents/AgentDashboard.tsx
+++ src/components/Agents/AgentDashboard.tsx
@@ -758,6 +758,7 @@ export function AgentDashboard() {
       {showTutorial && (
         <AgentTutorial
           onComplete={handleTutorialComplete}
+          onSkip={handleTutorialComplete}
         />
       )}
     </div>
